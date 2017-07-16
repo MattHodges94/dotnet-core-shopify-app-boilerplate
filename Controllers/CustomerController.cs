90afaccefddfb18e0d1963b5bb0a5d4d24e4bc14
Matt Hodges <matthewjhodges94@gmail.com>
2017-07-16 11:12:45 +0000

    init

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace shopify_app.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            var response = Program.GetCustomers();
+            return response;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(long id)
+        {
+            var response = Program.GetCustomer(id);
+            return response;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")] 
+        public void Put(long id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using RestSharp;
+using RestSharp.Authenticators;
+
+
+namespace shopify_app
+{
+    
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+
+            
+        }
+
+        public static String GetCustomer(long id){
+            
+            var client = new RestClient();
+            client.BaseUrl = new Uri("Enter shopify store url herre"); 
+            client.Authenticator = new HttpBasicAuthenticator("Enter API key here", "Enter API password here");
+            var request = new RestRequest(); 
+            request.Resource = System.String.Format("/admin/customers/{0}.json", id);
+            var response = client.ExecuteAsync(request);
+            return response.Result.Content;
+
+
+        }
+
+        public static String GetCustomers(){
+            var client = new RestClient();
+            client.BaseUrl = new Uri("Enter shopify store url herre"); 
+            client.Authenticator = new HttpBasicAuthenticator("Enter API key here", "Enter API password here");
+            var request = new RestRequest(); 
+            request.Resource = System.String.Format("/admin/customers.json");
+            var response = client.ExecuteAsync(request);
+            return response.Result.Content;
+        }
+
+    }
+
+    public static class RestClientExtensions
+{
+    public static async Task<RestResponse> ExecuteAsync(this RestClient client, RestRequest request)
+    {
+        TaskCompletionSource<IRestResponse> taskCompletion = new TaskCompletionSource<IRestResponse>();
+        RestRequestAsyncHandle handle = client.ExecuteAsync(request, r => taskCompletion.SetResult(r));
+        return (RestResponse)(await taskCompletion.Task);
+
+    }
+}
+}
